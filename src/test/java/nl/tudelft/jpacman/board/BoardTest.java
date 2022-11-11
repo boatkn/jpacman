1fb8ee5ee15923118d6298f9ffe324b5c87f7253
Krittitee Nildam <89009758+boatkn@users.noreply.github.com>
2022-11-11 10:47:35 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,24 +1,34 @@
 package nl.tudelft.jpacman.board;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
-import nl.tudelft.jpacman.sprite.Sprite;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Testing board class.
+ *
+ * @author Tasha Settewong 
+ */
 public class BoardTest {
+     /**
+     * Do the invarant method perform correctly?.
+     */
     @Test
-    void TestBasicSquare(){
-        Square[][] grid = {{new BasicSquare()}};
+    void testInvariant() {
+        Square row = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = row;
         Board board = new Board(grid);
-        assertThat(board.getHeight()).isEqualTo(1);
-        assertThat(board.getWidth()).isEqualTo(1);
+        assertThat(board.invariant()).isTrue();
     }
-
+    
+     /**
+     * Do the squareAt method perform correctly?.
+     */
     @Test
-    void TestsquareAt(){
-        Square[][] grid = {{null}};
+    void testSquareAt() {
+        Square row = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = row;
         Board board = new Board(grid);
-        assertThat(board.squareAt(0,1)).isEqualTo(0);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
     }
 }
